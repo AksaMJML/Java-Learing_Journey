0c0a0407a2d2a635f70e42eca1f4355053c01db9
AksaMJML <akaks0186@gmail.com>
2025-12-29 21:49:15 +0530

    tried equals n comparison at once in nested if

--- hello.java
+++ hello.java
@@ -1,14 +1,20 @@
-import java.lang.System;
+import java.util.*;
 
 class hello{
     public static void main(String args[]){
-        int num1 = 20;
-        int num2 = 76;
-
-        if(num1>num2){
-            System.out.println("Number 1 is Greater");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number 01 :");
+        int num1 = input.nextInt();
+        System.out.print("Enter Number 02 :");
+        int num2 = input.nextInt();
+        if(num1==num2){
+            System.out.println("Both Numbers are Equal");
         }else{
-            System.out.println("Number 2 is Greater");
+            if (num1>num2) {
+                System.out.println("Number 1 is Greater");
+            }else{
+                System.out.println("Number 2 is Greater");
+            }
         }
     }
 }
