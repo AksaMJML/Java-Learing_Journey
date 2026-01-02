3ed6007a88d9c7c716aacf496899beb4b6495264
AksaMJML <akaks0186@gmail.com>
2026-01-02 23:37:33 +0530

    understood the difference string variable n string object

--- JavaRevision/hello.java
+++ JavaRevision/hello.java
@@ -1,13 +1,1 @@
-import java.lang.System;
-import java.util.Scanner;
 
-class hello{
-    public static void main(String args[]){
-        boolean rain = true;
-        if(rain){
-            System.out.println("Take an Umbrella");
-        }else{
-            System.out.println("Enjoy the sunlight!");
-        }
-    }
-}
--- hello.java
+++ hello.java
@@ -2,19 +2,13 @@ import java.util.*;
 
 class hello{
     public static void main(String args[]){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter Number 01 :");
-        int num1 = input.nextInt();
-        System.out.print("Enter Number 02 :");
-        int num2 = input.nextInt();
-        if(num1==num2){
-            System.out.println("Both Numbers are Equal");
-        }else{
-            if (num1>num2) {
-                System.out.println("Number 1 is Greater");
-            }else{
-                System.out.println("Number 2 is Greater");
-            }
-        }
+      String a1 = "apple";
+      String a2 = "apple";
+
+      String f1 = new String("banana");
+      String f2 = new String("banana");
+
+      System.out.println(a1 == a2); // true, same reference in string pool
+      System.out.println(f1 == f2); // false, different references
     }
 }
