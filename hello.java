16feaf3f18fe0074303fdc0d2f9d041b02f97f76
AksaMJML <akaks0186@gmail.com>
2026-01-05 19:40:03 +0530

    equals method to check value

--- hello.java
+++ hello.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 
 public class hello {
 public static void main(String args[]){
-   String a1 = "re";
-      String a2 = "apple";
 
       String f1 = new String("banana");
       String f2 = new String("banana");
 
-      System.out.println(a1 == a2); // true, same reference in string pool
-      System.out.println(f1 == f2); // false, different references
+      System.out.println(f1.equals(f2)); //true, checks the value
     }
 }
