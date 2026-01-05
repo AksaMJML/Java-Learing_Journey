26537f3c51c55cc2cb3f3f95a7cb61863d2d23e9
AksaMJML <akaks0186@gmail.com>
2026-01-05 19:51:26 +0530

    check the sting pool and object reference

--- hello.java
+++ hello.java
@@ -5,10 +5,10 @@ public class hello {
 public static void main(String args[]){
 
       String a = "one";
-      String b = "one";
+      String b = new String("one");
 
       String c=b;
 
-      System.out.println(a==c); //true
+      System.out.println(a==c); //false , because a refers to string literal in string pool and c refers to object in heap memory
     }
 }
