98b3df5dd5128e0d68642b86f0e93c3c133491e7
AksaMJML <akaks0186@gmail.com>
2026-01-05 19:35:43 +0530

    string check the reference in this == function

--- hello.java
+++ hello.java
@@ -2,21 +2,14 @@ import java.util.*;
 import java.util.Scanner;
 
 public class hello {
+public static void main(String args[]){
+   String a1 = "re";
+      String a2 = "apple";
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Int: ");
-        int i = input.nextInt(); 
-        System.out.print("Double: ");
-        double d = input.nextDouble();
-        System.out.print("String: ");
-        String s = input.nextLine();
-        input.next();
-        
-        System.out.println("String : "+s); 
-        System.out.println("Double : "+d);
-        System.out.println("Int : "+i);
-        
+      String f1 = new String("banana");
+      String f2 = new String("banana");
+
+      System.out.println(a1 == a2); // true, same reference in string pool
+      System.out.println(f1 == f2); // false, different references
     }
 }
