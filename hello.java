2dc6ab3a7a04e4fbcf68e30b98ac9ec002c27bf1
AksaMJML <akaks0186@gmail.com>
2026-02-01 12:09:03 +0530

    tried getters setters manually

--- hello.java
+++ hello.java
@@ -1,10 +1,37 @@
-import java.util.*;
+class Sample {
+    private int x;
+    private int y;
+
+    public Sample(int x , int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public int getY(){
+        return y;
+    }
+}
 
 class Hello {
     public static void main(String[] args){
-        m();
+        Sample sample = new Sample(10 ,20);
+        System.out.println(sample.getX() + " " + sample.getY());
+        sample.setX(100);
+        System.out.println(sample.getX());
+        sample.setY(200);
+        System.out.println(sample.getY());
     }
-    public static void m(){
-        System.out.println("This is m method");
-    }
+    
 }
